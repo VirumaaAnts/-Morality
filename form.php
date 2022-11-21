ca97b5d00d23709b474b7ce05971458230bdabbf
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-11-21 08:27:48 +0200

    ANTS-10 Updated the consultation form

--- form.php
+++ form.php
@@ -1,7 +1,7 @@
 <?php
 $to      = 'maksim.dzjubenko@ivkhk.ee';
 $subject = 'Molarity+';
-$message = "Name: ".$_POST["name"]. "\r\n" ."Description: ".$_POST["description"];
+$message = "Name: ".$_POST["name"]. "\r\n"."Age: ".$_POST["age"]. "\r\n" ."Sex: ".$_POST["sex"]. "\r\n"."Description: ".$_POST["description"];
 $headers = array(
     'From' => $_POST["email"]
 );
